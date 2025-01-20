b8e474c9d40d4731257f67095e6a587e75abe5ec
STRAN1k228 <112329876+STRAN1k228@users.noreply.github.com>
2025-01-20 17:50:35 +0300

    Update main.py
    начал писать класс враг

--- main.py
+++ main.py
@@ -90,7 +90,13 @@ def start_game():
                 self.rect.x -= self.speed
             if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
                 self.rect.x += self.speed
-
+                
+    class Enemy(pygame.sprite.Sprite):
+        def __init__(self):
+            super().__init__()
+            self.image = enemy_image
+            self.rect = self.image.get_rect(center=(random.randint(0, WIDTH), 0))
+            self.speed = random.randint(2, 5)
 
 
 
